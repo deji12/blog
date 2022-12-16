0fd43138d0859e98a06d6e9a13b09e8edcce4e80
Ayodeji Adesola <82943610+deji12@users.noreply.github.com>
2022-12-16 17:44:37 +0800

    updaing for railway deployment

--- blogprj/settings.py
+++ blogprj/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
